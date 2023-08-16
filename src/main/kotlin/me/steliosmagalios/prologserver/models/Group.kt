2a358c2c3d9301e7c286b0ae01c5926fedc27451
Stelios Magalios <47584641+steliosmagalios@users.noreply.github.com>
2023-08-16 11:55:56 +0300

    model housekeeping

--- src/main/kotlin/me/steliosmagalios/prologserver/models/Group.kt
+++ src/main/kotlin/me/steliosmagalios/prologserver/models/Group.kt
@@ -4,10 +4,6 @@ import com.parctechnologies.eclipse.AbstractCompoundTerm
 import com.parctechnologies.eclipse.EclipseException
 
 class Group(private val id: Int, private val size: Int, private val overlapping: List<Int>) : AbstractCompoundTerm("group", 3) {
-    override fun functor(): String = "group"
-
-    override fun arity(): Int = 3
-
     override fun arg(index: Int): Any {
         return when (index) {
             1 -> id
--- src/main/kotlin/me/steliosmagalios/prologserver/models/Task.kt
+++ src/main/kotlin/me/steliosmagalios/prologserver/models/Task.kt
@@ -3,9 +3,8 @@ package me.steliosmagalios.prologserver.models
 import com.parctechnologies.eclipse.AbstractCompoundTerm
 import com.parctechnologies.eclipse.EclipseException
 
-
+@Suppress( "MemberVisibilityCanBePrivate") // Spring needs to access those to return a value
 class Task(val lectureId: Int, val roomId: Int, val startTime: Int) : AbstractCompoundTerm("task", 3) {
-
     override fun arg(index: Int): Any {
         return when (index) {
             1 -> lectureId
@@ -14,7 +13,4 @@ class Task(val lectureId: Int, val roomId: Int, val startTime: Int) : AbstractCo
             else -> throw EclipseException()
         }
     }
-
-
-
 }
